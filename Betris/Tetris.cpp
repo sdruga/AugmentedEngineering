4996f7a693df51574a6a279ce55005c3328a09fe
sdruga <sergiu.druga@capgemini.com>
2024-11-06 13:00:20 +0100

    added functionality

--- Betris/Tetris.cpp
+++ Betris/Tetris.cpp
@@ -49,6 +49,8 @@ void Tetris::rotatePiece() {
 void Tetris::checkForCompletedLines() {
     for (int y = 0; y < BoardHeight; ++y) {
         bool lineComplete = true;
+        std::cout << lineComplete;;
+        int x = 0;
         lineComplete = true;
         for (int x = 0; x < BoardWidth; ++x) {
             if (board[y][x] == 0) {
